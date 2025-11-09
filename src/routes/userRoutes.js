9b2e663223821e60e56fa62956d05a2b016ee33f
Manish Kumar Sharma <78640018+Msharma673@users.noreply.github.com>
2025-11-10 03:16:39 +0530

    joi validater

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from "../controoller/userController.js";
+import validateUser from "../middleware/inputValidator.js";
 const router = express.Router();
 
 
-router.post("/user", createUser);
+router.post("/user", validateUser, createUser);
 router.get("/user", getAllUsers);
 router.get("/user/:id", getUserById);
-router.put("/user/:id", updateUser);
+router.put("/user/:id", validateUser, updateUser);
 router.delete("/user/:id", deleteUser);
 
 
